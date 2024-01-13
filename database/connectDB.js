5d552d1f320d578b4a491c9bafbf65b3f8e9dd6a
Uttam Makwana <uttamakwana4503@gmail.com>
2024-01-13 09:40:06 +0530

    Change some code in connectDB()

--- database/connectDB.js
+++ database/connectDB.js
@@ -1,11 +1,15 @@
-//* import
 import mongoose from "mongoose";
 
-//* method
 const connectDB = async () => {
-  const database = await mongoose.connect(process.env.MONGO_URI);
-  console.log(database.connection.host);
-  console.log(database.connection.name);
+  try {
+    const database = await mongoose.connect(process.env.MONGO_URI);
+
+    console.log(`Connected to MongoDB at ${database.connection.host}`);
+    console.log(`Database name: ${database.connection.name}`);
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+    // You might want to handle the error appropriately (e.g., throw it, log it, etc.)
+  }
 };
 
 export default connectDB;
